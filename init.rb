b0e05ffcd97aaf13d1afebf4f55b9e91e3970587
Luke Parry <l.parry@warwick.ac.uk>
2011-07-23 15:28:25 +0100

    First Commit for plugin used to render redmine.openbuggi.org
    Signed-off-by: Luke Parry <l.parry@warwick.ac.uk>

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :openbuggi do
+  name 'OPENBUGGi Goodness'
+  author 'Luke Parry'
+  description 'Plugin to customise Redmine with OPENBUGGi goodness'
+  version '0.0.1'
+end
